1b63656756515b1e15e8471bf0f104d94930d14e
KSL78 <53367924+KSL78@users.noreply.github.com>
2023-01-11 01:39:23 +0900

    Update t.py

--- t.py
+++ t.py
@@ -2,3 +2,10 @@ print('aaaaaa')
 b = 'life '
 f = b[1:]
 print(f)
+print('good food %s' %'hamberger')
+print('good food %s' %b)
+print('error %10d%%'%20)
+print('error %-10d%%'%20)
+print('error %0.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
+print('error %10.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
+print('error %-10.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
