d533e7f8782413e1e43acfcc86a8f698a847b5af
KSL78 <53367924+KSL78@users.noreply.github.com>
2023-01-11 20:24:57 +0900

    Create namelistchanger.py

---
+++ namelistchanger.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+file_path = 'C:/Users/lks/Desktop/A'
+
+file_names = os.listdir(file_path)
+i = 1
+shutil.copytree(file_path,'C:/Users/lks/Desktop/co')
+for name in file_names:
+    src = os.path.join(file_path, name)
+    dst = str(i) + '.jpg'
+    dst = os.path.join(file_path, dst)
+    os.rename(src, dst)
+    i += 1
+###이름 변경 코어 모듈###
