188809a66c38839ecdb66ff1cbc25ab20d1588f3
KSL78 <53367924+KSL78@users.noreply.github.com>
2023-01-11 20:24:55 +0900

    Update t.py

--- t.py
+++ t.py
@@ -1,24 +1,8 @@
-print('aaaaaa')
-b = 'life '
-f = b[1:]
-print(f)
-print('good food %s' %'hamberger')
-print('good food %s' %b)
-print('error %10d%%'%20)
-print('error %-10d%%'%20)
-print('error %0.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
-print('error %10.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
-print('error %-10.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
-ae = [1,2,3,4,5,['an','den']]
-print(ae[:2])
-print(ae[:2])
-print(ae[:5])
-print(ae[5])
-print(ae[5][:1])
-del(ae[1])
-print(ae)
-dic = {"lev":45}
-print(dic['lev'])
-l = [1,2,3,4,5]
-z = tuple(l)
-print(z[:])
+class hendv:
+    def __init__(self,name):#기본생성자
+        self.name = name
+    def getname(self):
+        print(self.name)
+shi = hendv("shiv")
+#shi.setname("shiv")
+shi.getname()
