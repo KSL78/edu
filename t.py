0f3f9cebc3f70fc59be2bdb37fa1219b74355c29
KSL78 <53367924+KSL78@users.noreply.github.com>
2023-01-11 16:08:25 +0900

    Update t.py

--- t.py
+++ t.py
@@ -9,3 +9,11 @@ print('error %-10d%%'%20)
 print('error %0.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
 print('error %10.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
 print('error %-10.4f%%'%3.123456789)#꼭 실수값으로 하기 %d가 아님
+ae = [1,2,3,4,5,['an','den']]
+print(ae[:2])
+print(ae[:2])
+print(ae[:5])
+print(ae[5])
+print(ae[5][:1])
+del(ae[1])
+print(ae)
