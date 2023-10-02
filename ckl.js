7d21288e5e0a270f8d83c5107aafd9c0b3a11302
KSL78 <acc8041@gmail.com>
2023-10-02 17:49:02 +0900

    Delete ckl.js

--- ckl.js
+++
@@ -1,8 +0,0 @@
-const ckl = document.querySelector("p#ckl");
-function getClock()
-{
-    const date = new Date();
-    ckl.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-}
-getClock();
-setInterval(getClock,1000);
