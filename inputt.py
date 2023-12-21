8a77dc3d91f637f80dafec4be049fb6687e16695
KSL78 <acc8041@gmail.com>
2023-12-22 02:25:40 +0900

    Create inputt.py

---
+++ inputt.py
@@ -0,0 +1,14 @@
+#ad = input().split()
+#ad = list(input().split())
+# ad = list(map(int, input().split()))
+# print(ad)
+
+n, m = map(int, input().split())
+
+
+    
+#2
+mylist = []
+for i in range(n):
+	mylist.append(list(map(int, input().split())))
+print(mylist)
